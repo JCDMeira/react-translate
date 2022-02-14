b02f250aec09c7506ad0dcab59ba415943930626
Jean Meira <jean.meira10@hotmail.com>
2022-02-14 10:17:36 -0300

    feat: clear code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import './App.css';
+import { GlobalStyle, Conteiner } from './global';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Conteiner>
+        <h1>hello</h1>
+      </Conteiner>
+    </>
   );
 }
 
