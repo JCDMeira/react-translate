ea9b4d9bd98658cc72c747d5dfdb3445852942d6
Jean Meira <jean.meira10@hotmail.com>
2022-02-21 10:03:54 -0300

    feat: add class exemple

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,62 @@
-/*
-  Exemplo completo em um único arquivo usando a forma mais antiga (legado), com o translate
-
-*/
-
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { GlobalStyle, Conteiner } from './global';
-import { TranslatorProvider } from 'react-translate';
-import * as lang from './languages';
-import Home from './Components/Home';
+import { TranslatorProvider, translate } from 'react-translate';
 
-function App() {
-  const [langage, setLanguage] = useState(lang.languagePt);
+const languageEn = {
+  locale: 'en-US',
+  Home: {
+    HELLO: 'Hello world!',
+  },
+};
+const languagePt = {
+  locale: 'pt-BR',
+  Home: {
+    HELLO: 'Olá mundo!',
+  },
+};
 
-  const handleChange = ({ target: { value } }) => {
-    if (value === 'pt-BR') setLanguage(lang.languagePt);
-    if (value === 'en-US') setLanguage(lang.languageEn);
-    if (value === 'es-ES') setLanguage(lang.languageEs);
-    if (value === '中文 ') setLanguage(lang.languageCh);
-  };
+// eslint-disable-next-line react/prop-types
+let Home = function ({ t }) {
+  return <h1> {t('HELLO')} </h1>;
+};
 
-  return (
-    <>
-      <GlobalStyle />
-      <Conteiner>
-        <TranslatorProvider translations={langage}>
-          <Home />
-        </TranslatorProvider>
-        <br />
-        <select onChange={(e) => handleChange(e)}>
-          <option value="pt-BR">PT</option>
-          <option value="en-US">EN</option>
-          <option value="es-ES">ES</option>
-          <option value="中文 ">中文</option>
-        </select>
-      </Conteiner>
-    </>
-  );
+Home = translate('Home')(Home);
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: 'languageEn',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    this.setState({
+      language:
+        this.state.language === 'languageEn' ? 'languagePt' : 'languageEn',
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <Conteiner>
+          <TranslatorProvider
+            translations={
+              this.state.language === 'languageEn' ? languagePt : languageEn
+            }
+          >
+            <Home />
+          </TranslatorProvider>
+
+          <select onChange={this.handleChange}>
+            <option value="pt-BR">PT</option>
+            <option value="en-us">EN</option>
+          </select>
+        </Conteiner>
+      </>
+    );
+  }
 }
-
-export default App;
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,14 +1,16 @@
 /*
-Usa um arquivo centralizado que força o arquivo a depender da
-biblioteca, enquanto o projeto depende apenas do arquivo próprio.
-em caso de mudanças e maioria das vezes é só mudar o arquivo
-próprio e não mexer em toda a aplicação.
+  É possível usar a forma legado sem reatribuir o componente
+  passando por translate. Para isso é só exportar ele com o
+  translate já sendo evocado. Dessa forma o componente terá naturalmente o
+  acesso a prop t, que pode ser usada no escopo da função.
 */
-import React from 'react';
-import useTextTranslate from '../../hooks/useTextTranslate.hook';
 
-function Home() {
-  return <h1> {useTextTranslate('Home', 'HELLO')} </h1>;
+import React from 'react';
+import { translate } from 'react-translate';
+
+// eslint-disable-next-line react/prop-types
+function Home({ t }) {
+  return <h1> {t('HELLO')} </h1>;
 }
 
-export default Home;
+export default translate('Home')(Home);
